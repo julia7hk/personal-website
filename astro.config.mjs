cd9f6acafbcfe8f203022bf7d5766a9fe74a9903
Julia Kang <julia7hk@gmail.com>
2025-09-29 01:15:56 -0400

    configure dns routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://julia7hk.github.io',
-	base: '/personal-website',
+	site: 'https://julia7hk.com',
+	base: '/',
 });
